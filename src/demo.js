e27bb1cd2e1fbac01bb0ecd3141151a09018c3c2
Airthe911 <1174617612@qq.com>
2023-01-23 23:37:40 +0800

    添加了网页模板，增加了开关麦克风功能

--- src/demo.js
+++ src/demo.js
@@ -1,86 +1,59 @@
 import TRTC from 'trtc-js-sdk';
-import "./demo.css";
-let sdkAppId = 1400789272; // '填入您创建应用的 sdkAppId'
-
-let roomId ; // '您指定的房间号'
-
-let userId ; // '您指定的用户ID'
-
-let userSig ; // '生成的userSig'
-
+let sdkAppId = 1400789272
+let roomId
+let userId
+let userSig
 let client, localStream;
-
 document.getElementById("startCall").onclick = async function () {
   roomId = parseInt(document.querySelector('#roomId').value);
-
   userId = document.querySelector('#userId').value;
-
   userSig = document.querySelector('#userSig').value;
-
   client = TRTC.createClient({ mode: 'rtc', sdkAppId, userId, userSig });
-
   // 1.监听事件
-
   client.on('stream-added', event => {
-
     const remoteStream = event.stream;
-
     console.log('远端流增加: ' + remoteStream.getId());
-
     //订阅远端流
-
     client.subscribe(remoteStream);
-
   });
-
   client.on('stream-subscribed', event => {
-
     // 远端流订阅成功
-
     const remoteStream = event.stream;
-
-    // 播放远端流，传入的元素 ID 必须是页面里存在的 div 元素
-
+    // 播放远端流
     remoteStream.play('remoteStreamContainer');
-
   });
-
   // 2.进房成功后开始推流
-
   try {
-
     await client.join({ roomId });
-
-    localStream = TRTC.createStream({ userId, audio: true, video: true });
-
+    localStream = TRTC.createStream({ userId, audio: true, video: false });
     await localStream.initialize();
-
     // 播放本地流
-
     localStream.play("localStreamContainer");
-
     await client.publish(localStream);
-
   } catch (error) {
-
     console.error(error);
-
   }
-
 }
 
 document.getElementById("finishCall").onclick = async function () {
-
   // 停止本地流预览
-
   localStream.close();
-
   await client.leave();
-
-  // 退房成功，如果没有调用 client.destroy()，可再次调用 client.join 重新进房开启新的通话
-
-  // 调用 destroy() 结束当前 client 的生命周期
-
+  // 调用 destroy() 结束当前client
   client.destroy();
+}
 
+document.getElementById("shutdown_mic").onclick = async function() { //用于关闭麦克风
+  localStream.muteAudio();
+  const audioTrack = localStream.getAudioTrack();
+  if (audioTrack) {
+    audioTrack.stop();
+  }
+}
+
+document.getElementById("turnon_mic").onclick = async function() { //用于打开麦克风
+  const stream = TRTC.createStream({ audio: true, video:false });
+  await stream.initialize();
+  localStream.unmuteAudio();
+  await localStream.replaceTrack(stream.getAudioTrack());
 }
